18ffc46814d40c06bb35714b23ef247ab26ff54e
Documenter.jl <documenter@juliadocs.github.io>
2022-12-19 13:14:23 +0000

    build based on fb96c5d

---
+++ dev/search_index.js
@@ -0,0 +1,3 @@
+var documenterSearchIndex = {"docs":
+[{"location":"","page":"Home","title":"Home","text":"CurrentModule = SparseIdentification","category":"page"},{"location":"#SparseIdentification","page":"Home","title":"SparseIdentification","text":"","category":"section"},{"location":"","page":"Home","title":"Home","text":"Documentation for SparseIdentification.","category":"page"},{"location":"","page":"Home","title":"Home","text":"","category":"page"},{"location":"","page":"Home","title":"Home","text":"Modules = [SparseIdentification]","category":"page"}]
+}
