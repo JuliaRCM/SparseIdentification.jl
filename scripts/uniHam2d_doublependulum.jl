6f92d7e1e817d56fe286f5ff9a0b3fca524d26a7
Nigel Bruce Khan <nigelbrk@gmail.com>
2023-02-22 13:51:58 +0100

    added code for new loss function, and a pendulum example

---
+++ scripts/uniHam2d_doublependulum.jl
@@ -0,0 +1,171 @@
+#This file uses uniform sampling to find the SINDY solution of a hamiltonian system in 1D and 2D
+
+using DifferentialEquations
+using Distributions
+using ODE
+using Plots
+using Random
+using SparseIdentification
+using Zygote
+using ForwardDiff
+using Optim
+
+gr()
+
+
+# --------------------
+# Setup
+# --------------------
+
+println("Setting up...")
+
+# define the number of variables, q,p in this case gives 2 variables
+const d = 1
+
+#############################################################
+#############################################################
+# IMP SETUP NOTE: 2D system with 4 variables [q₁, q₂, p₁, p₂]
+const nd = 4d
+#############################################################
+#############################################################
+
+# search space up to polyorder polynomials (highest polynomial order)
+const polyorder = 3 
+
+######################################################################
+######################################################################
+# maximum wave number of trig basis for function library to explore
+# trig_wave_num can be adjusted if higher frequency arguments expected
+const trig_wave_num = 0
+######################################################################
+######################################################################
+
+# 1 dim each of [q₁, q₂, p₁, p₂] gives 4*d = 4 variables
+out = zeros(nd)
+
+# initialize analytical function, keep ϵ bigger than lambda so system is identifiable
+ϵ = 0.5
+m = 1 # m₁=m₂
+l₁ = 1
+l₂ = 1
+g = 9.81
+
+h₁(x) = (x[3]*x[4]*sin(x[1]-x[2])) / (l₁*l₂*(m + m*(sin(x[1]-x[2]))^2))
+h₂(x) = m * l₂^2 * x[3]^2 + (m+m) * l₁^2 * x[4]^2 - 2*m*l₁*l₂*x[3]*x[4]*cos(x[1]-x[2]) /
+        2*l₁^2 *l₂^2 (m + m * (sin(x[1]-x[2]))^2)^2
+
+
+# two-dim simple harmonic oscillator (not used anywhere only in case some testing needed)
+# H_ana(x, p, t) = ϵ * x[1]^2 + ϵ * x[2]^2 + 1/(2*m) * x[3]^2 1/(2*m) * x[4]^2
+
+# Gradient function of the 2D hamiltonian
+#grad_H_ana(x) = [x[3]; x[4]; -2ϵ * x[1]; -2ϵ * x[2]]
+#grad_H_ana(x) = [x[3]; x[4]; sin(x[1]); sin(x[2])]
+grad_H_ana(x) = [(l₂*x[3] - l₁*x[4] * cos(x[1]-x[2])) / (l₁^2 * l₂ * (m + m*(sin(x[1]-x[2]))^2)); 
+                 (-m*l₂*x[3] * cos(x[1]-x[2]) + (m+m)*(l₁*x[4])) / (m * l₁ * l₂^2 * (m + m*(sin(x[1]-x[2]))^2));
+                 -(m+m)*g*l₁*sin(x[1]) - h₁(x) + h₂(x) * sin(2*(x[1]-x[2]));
+                 -m*g*l₂*sin(x[2]) + h₁(x) - h₂(x) * sin(2*(x[1]-x[2]))];
+grad_H_ana(x, p, t) = grad_H_ana(x)
+
+# ------------------------------------------------------------
+# Training Data
+# ------------------------------------------------------------
+
+println("Generate Training Data...")
+
+# number of samples
+num_samp = 8
+
+# samples in p and q space
+samp_range = LinRange(-20, 20, num_samp)
+
+# initialize vector of matrices to store ODE solve output
+
+# compute vector field from x state values
+# stored as matrix with dims [nd,ntime]
+x = zeros(nd, num_samp^nd)
+ẋ = zero(x)
+s = collect(Iterators.product(samp_range,samp_range, samp_range, samp_range))
+
+for j in eachindex(s)
+    x[:,j] .= s[j]
+    ẋ[:,j] .= grad_H_ana(x[:,j])
+end
+
+# collect training data
+tdata = TrainingData(x, ẋ)
+
+
+# ----------------------------------------
+# Compute Sparse Regression
+# ----------------------------------------
+
+# choose SINDy method
+# (lambda parameter must be close to noise value so that only coeffs with value around the noise are sparsified away)
+method = HamiltonianSINDy(lambda = 0.05, noise_level = 0.05, polyorder = polyorder, trigonometric = trig_wave_num)
+
+# compute vector field
+vectorfield = VectorField(method, tdata)
+
+println(vectorfield.coefficients)
+
+
+# ----------------------------------------
+# Plot Results
+# ----------------------------------------
+
+println("Plotting...")
+
+# ----------------------------------------
+# Plot error in approximate gradient
+# ----------------------------------------
+
+println("Compute approximate gradient...")
+
+ẋid = zero(ẋ)
+
+for j in axes(ẋid, 2)
+    @views vectorfield(ẋid[:,j], x[:,j])
+end
+
+
+# ----------------------------------------
+# Plot some solutions
+# ----------------------------------------
+
+tstep = 0.01
+tspan = (0.0,25.0)
+trange = range(tspan[begin], step = tstep, stop = tspan[end])
+
+for i in 1:5
+    idx = rand(1:length(s))
+
+    prob_reference = ODEProblem(grad_H_ana, x[:,idx], tspan)
+    data_reference = ODE.solve(prob_reference, Tsit5(), abstol=1e-10, reltol=1e-10, saveat = trange, tstops = trange)
+
+    prob_sindy = ODEProblem(vectorfield, x[:,idx], tspan)
+    data_sindy = ODE.solve(prob_sindy, Tsit5(), abstol=1e-10, reltol=1e-10, saveat = trange, tstops = trange) 
+
+    p1 = plot(xlabel = "Time", ylabel = "q₁")
+    scatter!(p1, data_reference.t, data_reference[1,:], label = "Data q₁")
+    scatter!(p1, data_sindy.t, data_sindy[1,:], markershape=:xcross, label = "Identified q₁")
+
+    p3 = plot(xlabel = "Time", ylabel = "p₁")
+    scatter!(p3, data_reference.t, data_reference[3,:], label = "Data p₁")
+    scatter!(p3, data_sindy.t, data_sindy[3,:], markershape=:xcross, label = "Identified p₁")
+
+    plot!(size=(1000,1000))
+    display(plot(p1, p3, title="Analytical vs Calculated q₁ & p₁ in a 2D system with Euler"))
+
+    p2 = plot(xlabel = "Time", ylabel = "q₂")
+    scatter!(p2, data_reference.t, data_reference[2,:], label = "Data q₂")
+    scatter!(p2, data_sindy.t, data_sindy[2,:], markershape=:xcross, label = "Identified q₂")
+
+    p4 = plot(xlabel = "Time", ylabel = "p₂")
+    scatter!(p4, data_reference.t, data_reference[4,:], label = "Data p₂")
+    scatter!(p4, data_sindy.t, data_sindy[4,:], markershape=:xcross, label = "Identified p₂")
+
+    plot!(size=(1000,1000))
+    display(plot(p2, p4, title="Analytical vs Calculated q₂ & p₂ in a 2D system with Euler"))
+
+end
--- src/methods/hamiltonian_sindy.jl
+++ src/methods/hamiltonian_sindy.jl
@@ -110,7 +110,7 @@ function VectorField(method::HamiltonianSINDy, data::TrainingData; solver = Newt
     ∇H = hamilGrad_func_builder(d, method.polyorder, method.trigonometric)
 
     # Compute Sparse Regression
-    coeffs = sparsify(method, ∇H, data.x, data.ẋ, solver)
+    coeffs = sparsify_two(method, ∇H, data.x, data.ẋ, solver)
 
     HamiltonianSINDyVectorField(coeffs, ∇H)
 end
@@ -126,3 +126,119 @@ function (vectorfield::HamiltonianSINDyVectorField)(dz, z)
 end
 
 (vectorfield::HamiltonianSINDyVectorField)(dz, z, p, t) = vectorfield(dz, z)
+
+
+
+
+
+
+
+
+
+
+
+
+################################################################################################
+################################################################################################
+################################################################################################
+################################################################################################
+function sparsify_two(method::HamiltonianSINDy, ∇H, x, ẋ, solver)
+    # add noise
+    ẋnoisy = ẋ .+ method.ϵ .* randn(size(ẋ))
+
+    # dimension of system
+    nd = size(x,1)
+
+    # binomial used to get the combination of variables till the highest order without repeat, nparam = 34 for 3rd order, with z = q,p each of 2 dims
+    nparam = calculate_nparams(nd, method.polyorder, method.trigonometric)
+
+    # (a) initialized to a vector of zeros b/c easier to optimze zeros for our case
+    coeffs = zeros(nparam)
+    
+    # define loss function
+    function loss(a::AbstractVector)
+
+        # initialization for euler solution
+        eulerX = zeros(eltype(a), axes(x))
+        EulerTimeStep = 0.01
+
+        # initialization for anaylitical solution
+        tspan = (0.0, 0.01)
+        trange = range(tspan[begin], step = EulerTimeStep, stop = tspan[end])
+        
+        data_ref = zero(x)
+
+        # Gradient function of the 2D hamiltonian
+        ϵ = 0.5
+        m = 1 # m₁=m₂
+        l₁ = 1
+        l₂ = 1
+        g = 9.81
+
+        h₁(x) = (x[3]*x[4]*sin(x[1]-x[2])) / (l₁*l₂*(m + m*(sin(x[1]-x[2]))^2))
+        h₂(x) = m * l₂^2 * x[3]^2 + (m+m) * l₁^2 * x[4]^2 - 2*m*l₁*l₂*x[3]*x[4]*cos(x[1]-x[2]) /
+        2*l₁^2 *l₂^2 (m + m * (sin(x[1]-x[2]))^2)^2
+
+        grad_H_ana(x) = [(l₂*x[3] - l₁*x[4] * cos(x[1]-x[2])) / (l₁^2 * l₂ * (m + m*(sin(x[1]-x[2]))^2)); 
+                 (-m*l₂*x[3] * cos(x[1]-x[2]) + (m+m)*(l₁*x[4])) / (m * l₁ * l₂^2 * (m + m*(sin(x[1]-x[2]))^2));
+                 -(m+m)*g*l₁*sin(x[1]) - h₁(x) + h₂(x) * sin(2*(x[1]-x[2]));
+                 -m*g*l₂*sin(x[2]) + h₁(x) - h₂(x) * sin(2*(x[1]-x[2]))];
+
+        #grad_H_ana(x) = [x[3]; x[4]; -2ϵ * x[1]; -2ϵ * x[2]]
+
+        grad_H_ana(x, p, t) = grad_H_ana(x)
+
+        # initialization for the SINDy coefficients result
+        res = zeros(eltype(a), axes(ẋnoisy))
+        out = zeros(eltype(a), nd)
+        
+        for j in axes(res, 2)
+            ∇H(out, x[:,j], a)
+            res[:,j] .= out
+            eulerX[:,j] .= x[:,j] + EulerTimeStep * res[:,j]
+
+            prob_ref = ODEProblem(grad_H_ana, x[:,j], tspan)
+            sol = ODE.solve(prob_ref, Tsit5(), dt = 0.01, abstol = 1e-10, reltol = 1e-10, saveat = trange)
+            data_ref[:,j] = sol.u[2]
+        end
+
+        mapreduce(y -> y^2, +, data_ref .- eulerX)
+    end
+    
+    # initial guess
+    println("Initial Guess...")
+    result = Optim.optimize(loss, coeffs, solver; autodiff = :forward)
+    coeffs .= result.minimizer
+
+    println(result)
+
+    for n in 1:method.nloops
+        println("Iteration #$n...")
+
+        # find coefficients below λ threshold
+        smallinds = abs.(coeffs) .< method.λ
+        biginds = .~smallinds
+
+        # check if there are any small coefficients != 0 left
+        all(coeffs[smallinds] .== 0) && break
+
+        # set all small coefficients to zero
+        coeffs[smallinds] .= 0
+
+        # Regress dynamics onto remaining terms to find sparse a
+
+        function sparseloss(b::AbstractVector)
+            c = zeros(eltype(b), axes(coeffs))
+            c[biginds] .= b
+            loss(c)
+        end
+
+        b = coeffs[biginds]
+        result = Optim.optimize(sparseloss, b, solver; autodiff = :forward)
+        b .= result.minimizer
+
+        println(result)
+    end
+    
+    return coeffs
+end
