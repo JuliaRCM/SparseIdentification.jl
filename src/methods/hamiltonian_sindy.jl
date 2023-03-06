b4f4a66f32faac9657a913fafcdbef22c9fe76a2
nigel-bruce-khan <nigelbrk@gmail.com>
2023-03-06 16:22:34 +0100

    need to add autoencoder, fix sparsify_two x_dot noise part

--- src/methods/hamiltonian_sindy.jl
+++ src/methods/hamiltonian_sindy.jl
@@ -145,6 +145,8 @@ end
 ################################################################################################
 ################################################################################################
 function sparsify_two(method::HamiltonianSINDy, ∇H, x, ẋ, solver)
+
+    # TODO: add noise to data_ref at mapreduce stage and not here
     # add noise
     ẋnoisy = ẋ .+ method.noise_level .* randn(size(ẋ))
 
@@ -154,7 +156,7 @@ function sparsify_two(method::HamiltonianSINDy, ∇H, x, ẋ, solver)
     # binomial used to get the combination of variables till the highest order without repeat, nparam = 34 for 3rd order, with z = q,p each of 2 dims
     nparam = calculate_nparams(nd, method.polyorder, method.trigonometric)
 
-    # (a) initialized to a vector of zeros b/c easier to optimze zeros for our case
+    # coeffs initialized to a vector of zeros b/c easier to optimze zeros for our case
     coeffs = zeros(nparam)
     
     # define loss function
@@ -188,6 +190,8 @@ function sparsify_two(method::HamiltonianSINDy, ∇H, x, ẋ, solver)
 
             prob_ref = ODEProblem(method.analytical_∇H, x[:,j], tspan)
             sol = ODE.solve(prob_ref, Tsit5(), dt = EulerTimeStep, abstol = 1e-10, reltol = 1e-10, saveat = trange)
+
+            # sol.u[1] is one of the uniform sampled initial conditions we give, sol.u[2] is the predicted set of values at the EulerTimeStep
             data_ref[:,j] = sol.u[2]
         end
 
@@ -230,4 +234,118 @@ function sparsify_two(method::HamiltonianSINDy, ∇H, x, ẋ, solver)
     end
     
     return coeffs
+end
+
+
+
+
+
+
+
+################################################################################################
+################################################################################################
+################################################################################################
+################################################################################################
+
+# TODO: 
+# 1. find SINDY graident function on reduced dims, already decide on size of reduced dims
+    # 1.1 find also nparam and coeffs on reduced dims
+# 2. use autoencoder to reduce dims i.e less numbers of q,p
+# 3. calculate x using SINDY method
+# 4. reverse autoencoder to actual variables
+# 5. find loss function
+function sparsify_three(method::HamiltonianSINDy, ∇H, x, ẋ, solver)
+
+# TODO: remove ẋ, it is unused
+
+   # dimension of system
+   nd = size(x,1)
+
+   # binomial used to get the combination of variables till the highest order without repeat, nparam = 34 for 3rd order, with z = q,p each of 2 dims
+   nparam = calculate_nparams(nd, method.polyorder, method.trigonometric)
+
+   # coeffs initialized to a vector of zeros b/c easier to optimze zeros for our case
+   coeffs = zeros(nparam)
+   
+   # define loss function
+   function loss(a::AbstractVector)
+
+       #TODO: add call to autoencoder:encoder part
+       encoder = Dense(4, 4, relu)
+       
+       # initialization for euler solution
+       hermiteX = zeros(eltype(a), axes(x))
+       EulerTimeStep = 0.01 # random choice for last step
+       numLoops = 4 # random choice of loop steps
+
+       # initialization for anaylitical solution
+       tspan = (0.0, EulerTimeStep)
+       trange = range(tspan[begin], step = EulerTimeStep, stop = tspan[end])
+       
+       data_ref = zero(x)
+
+       # initialization for the SINDy coefficients result
+       res = zeros(eltype(a), axes(ẋnoisy))
+       out = zeros(eltype(a), nd)
+       
+       for j in axes(res, 2)
+           ∇H(out, x[:,j], a) # gradient at current (x) values
+           res[:,j] .= out
+           hermiteX[:,j] .= x[:,j] .+ EulerTimeStep .* res[:,j] # for first guess use explicit euler
+           
+           for loop = 1:numLoops
+               ∇H(out, (x[:,j] .+ hermiteX[:,j]) ./ 2, a) # find gradient at {(x̃ₙ + x̃ⁱₙ₊₁)/2} to get Hermite extrapolation
+               res[:,j] .= out
+               hermiteX[:,j] .= x[:,j] + EulerTimeStep * res[:,j] # mid point rule for integration to next step
+           end
+
+           prob_ref = ODEProblem(method.analytical_∇H, x[:,j], tspan)
+           sol = ODE.solve(prob_ref, Tsit5(), dt = EulerTimeStep, abstol = 1e-10, reltol = 1e-10, saveat = trange)
+
+           # sol.u[1] is one of the uniform sampled initial conditions we give, sol.u[2] is the predicted set of values at the EulerTimeStep
+           data_ref[:,j] = sol.u[2]
+       end
+
+       # add noise
+       data_ref_noisy = data_ref .+ method.noise_level .* randn(size(x))
+
+       mapreduce(y -> y^2, +, data_ref_noisy .- hermiteX)
+   end
+   
+   # initial guess
+   println("Initial Guess...")
+   result = Optim.optimize(loss, coeffs, solver, Optim.Options(show_trace=true); autodiff = :forward)
+   coeffs .= result.minimizer
+
+   println(result)
+
+   for n in 1:method.nloops
+       println("Iteration #$n...")
+
+       # find coefficients below λ threshold
+       smallinds = abs.(coeffs) .< method.λ
+       biginds = .~smallinds
+
+       # check if there are any small coefficients != 0 left
+       all(coeffs[smallinds] .== 0) && break
+
+       # set all small coefficients to zero
+       coeffs[smallinds] .= 0
+
+       # Regress dynamics onto remaining terms to find sparse a
+
+       function sparseloss(b::AbstractVector)
+           c = zeros(eltype(b), axes(coeffs))
+           c[biginds] .= b
+           loss(c)
+       end
+
+       b = coeffs[biginds]
+       result = Optim.optimize(sparseloss, b, solver, Optim.Options(show_trace=true); autodiff = :forward)
+       b .= result.minimizer
+
+       println(result)
+   end
+   
+   return coeffs
 end
