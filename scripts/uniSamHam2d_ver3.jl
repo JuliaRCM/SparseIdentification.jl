1daf5e106b90265e422c6c822fd5b9797bc13339
Nigel Bruce Khan <nigelbrk@gmail.com>
2023-04-09 02:56:27 +0200

    removed extra packages, extra variables, changed how s variable calculated, added comments, TODOs

--- scripts/uniSamHam2d_ver3.jl
+++ scripts/uniSamHam2d_ver3.jl
@@ -1,13 +1,11 @@
 #This file uses uniform sampling to find the SINDY solution of a hamiltonian system in 1D and 2D
 
 using DifferentialEquations
-using Distributions
 using ODE
+using Distributions
 using Plots
 using Random
 using SparseIdentification
-using Zygote
-using ForwardDiff
 using Optim
 
 gr()
@@ -19,36 +17,27 @@ gr()
 
 println("Setting up...")
 
-# define the number of variables, q,p in this case gives 2 variables
-const d = 1
-
-#############################################################
-#############################################################
-# IMP SETUP NOTE: 2D system with 4 variables [q₁, q₂, p₁, p₂]
-const nd = 4d
-#############################################################
-#############################################################
+# 2D system with 4 variables [q₁, q₂, p₁, p₂]
+const nd = 4
 
 # search space up to polyorder polynomials (highest polynomial order)
 const polyorder = 3 
 
-######################################################################
-######################################################################
 # maximum wave number of trig basis for function library to explore
 # trig_wave_num can be adjusted if higher frequency arguments expected
 const trig_wave_num = 2
-######################################################################
-######################################################################
 
-# 1 dim each of [q₁, q₂, p₁, p₂] gives 4*d = 4 variables
-out = zeros(nd)
+# maximum power of state difference basis for function library to explore
+#TODO: make diffs_power into vector later with start and end values to include negative powers
+const diffs_power = 1
 
-# initialize analytical function, keep ϵ bigger than λ so system is identifiable
+# initialize analytical function, keep λ smaller than ϵ so system is identifiable
 ϵ = 0.5
 m = 1
 
 # two-dim simple harmonic oscillator (not used anywhere only in case some testing needed)
-# H_ana(x, p, t) = ϵ * x[1]^2 + ϵ * x[2]^2 + 1/(2*m) * x[3]^2 1/(2*m) * x[4]^2
+# H_ana(x, p, t) = ϵ * x[1]^2 + ϵ * x[2]^2 + 1/(2*m) * x[3]^2 + 1/(2*m) * x[4]^2
+# TODO: check to confirm: H_ana(x, p, t) = cos(x[1]) + cos(x[2]) + 1/(2*m) * x[3]^2 + 1/(2*m) * x[4]^2
 
 # Gradient function of the 2D hamiltonian
 # grad_H_ana(x) = [x[3]; x[4]; -2ϵ * x[1]; -2ϵ * x[2]]
@@ -62,15 +51,17 @@ grad_H_ana(x, p, t) = grad_H_ana(x)
 println("Generate Training Data...")
 
 # number of samples
-num_samp = 12
+num_samp = 10
 
 # samples in p and q space
 samp_range = LinRange(-20, 20, num_samp)
 
 # initialize vector of matrices to store ODE solve output
 
-# s depend on size of nd (dimensions), 4 in the case here so we use samp_range x samp_range x samp_range x samp_range
-s = collect(Iterators.product(samp_range, samp_range, samp_range, samp_range))
+# s depend on size of nd (total dims), 4 in the case here so we use samp_range x samp_range x samp_range x samp_range
+# s = collect(Iterators.product(samp_range, samp_range, samp_range, samp_range))
+s = collect(Iterators.product(fill(samp_range, nd)...)) # TODO: is this way better to get s 
+
 
 # compute vector field from x state values
 x = [collect(s[i]) for i in eachindex(s)]
@@ -84,9 +75,10 @@ ẋ = [grad_H_ana(_x) for _x in x]
 # choose SINDy method
 # (λ parameter must be close to noise value so that only coeffs with value around the noise are sparsified away)
 # integrator_timeStep chosen randomly for now
-method = HamiltonianSINDy(grad_H_ana, λ = 0.05, noise_level = 0.05, integrator_timeStep = 0.01, polyorder = polyorder, trigonometric = trig_wave_num)
+method = HamiltonianSINDy(grad_H_ana, λ = 0.05, noise_level = 0.05, integrator_timeStep = 0.05, 
+                            polyorder = polyorder, trigonometric = trig_wave_num, diffs_power = diffs_power)
 
-# generate noisy references data
+# generate noisy references data at next time step
 y = SparseIdentification.gen_noisy_ref_data(method, x)
 
 # collect training data
@@ -111,8 +103,9 @@ println("Plotting...")
 println("Compute approximate gradient...")
 
 ẋid = zero.(ẋ)
-
+#TODO: following loop is not used to do anything i think
 for j in eachindex(ẋid)
+    # store the gradient of the identified vector field at x[j] into ẋid[j]
     vectorfield(ẋid[j], x[j])
 end
 
@@ -125,8 +118,8 @@ tstep = 0.01
 tspan = (0.0,25.0)
 trange = range(tspan[begin], step = tstep, stop = tspan[end])
 
-for i in 1:5
-    idx = rand(1:length(s))
+for _ in 1:5
+    idx = rand(1:length(s)) # chosse a random sample to plot
 
     prob_reference = ODEProblem(grad_H_ana, x[idx], tspan)
     data_reference = ODE.solve(prob_reference, Tsit5(), abstol=1e-10, reltol=1e-10, saveat = trange, tstops = trange)
