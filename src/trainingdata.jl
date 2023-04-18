3dfb89305c4cf2cc8ed807e59f5ee53a9390d432
Michael Kraus <michael.kraus@ipp.mpg.de>
2023-04-18 11:37:02 +0200

    Add TraningData constructors to allow for passing only two datasets.

--- src/trainingdata.jl
+++ src/trainingdata.jl
@@ -3,5 +3,7 @@ struct TrainingData{AT<:AbstractArray}
     x::AT # initial condition
     ẋ::AT # initial condition
     y::AT # noisy data at next time step
-end
 
+    TrainingData(x::AT, ẋ::AT, y::AT) where {AT} = new{AT}(x, ẋ, y)
+    TrainingData(x::AT, ẋ::AT) where {AT} = new{AT}(x, ẋ)
+end
