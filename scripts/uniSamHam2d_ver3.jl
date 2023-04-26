89b2237f722858cf837697c70baef5994313af6b
Nigel Bruce Khan <nigelbrk@gmail.com>
2023-04-26 14:43:06 +0200

    removed extra package calls, extra comments, extra code, changed how variable s is defined to make it more general

--- scripts/uniSamHam2d_ver3.jl
+++ scripts/uniSamHam2d_ver3.jl
@@ -1,14 +1,14 @@
-#This file uses uniform sampling to find the SINDY solution of a hamiltonian system in 1D and 2D
+#This file uses uniform sampling to find the SINDy solution of a hamiltonian system in 1D and 2D
 
-using DifferentialEquations
-using ODE
 using Distributions
+using GeometricIntegrators
+using Optim
 using Plots
 using Random
 using SparseIdentification
-using Optim
 
 gr()
+
 
 
 # --------------------
@@ -35,7 +35,7 @@ m = 1
 
 # two-dim simple harmonic oscillator (not used anywhere only in case some testing needed)
 # H_ana(x, p, t) = ϵ * x[1]^2 + ϵ * x[2]^2 + 1/(2*m) * x[3]^2 + 1/(2*m) * x[4]^2
-# TODO: check to confirm: H_ana(x, p, t) = cos(x[1]) + cos(x[2]) + 1/(2*m) * x[3]^2 + 1/(2*m) * x[4]^2
+# H_ana(x, p, t) = cos(x[1]) + cos(x[2]) + 1/(2*m) * x[3]^2 + 1/(2*m) * x[4]^2
 
 # Gradient function of the 2D hamiltonian
 # grad_H_ana(x) = [x[3]; x[4]; -2ϵ * x[1]; -2ϵ * x[2]]
@@ -57,8 +57,7 @@ samp_range = LinRange(-20, 20, num_samp)
 # initialize vector of matrices to store ODE solve output
 
 # s depend on size of nd (total dims), 4 in the case here so we use samp_range x samp_range x samp_range x samp_range
-# s = collect(Iterators.product(samp_range, samp_range, samp_range, samp_range))
-s = collect(Iterators.product(fill(samp_range, nd)...)) # TODO: is this way better to get s 
+s = collect(Iterators.product(fill(samp_range, nd)...))
 
 
 # compute vector field from x state values
@@ -94,55 +93,37 @@ println(vectorfield.coefficients)
 
 println("Plotting...")
 
-# ----------------------------------------
-# Plot error in approximate gradient
-# ----------------------------------------
-
-println("Compute approximate gradient...")
-
-ẋid = zero.(ẋ)
-#TODO: following loop is not used to do anything i think
-for j in eachindex(ẋid)
-    # store the gradient of the identified vector field at x[j] into ẋid[j]
-    vectorfield(ẋid[j], x[j])
-end
-
-
-# ----------------------------------------
-# Plot some solutions
-# ----------------------------------------
-
 tstep = 0.01
 tspan = (0.0,25.0)
 trange = range(tspan[begin], step = tstep, stop = tspan[end])
 
-for _ in 1:5
-    idx = rand(1:length(s)) # chosse a random sample to plot
+for i in 1:5
+    idx = rand(1:length(s))
 
-    prob_reference = DifferentialEquations.ODEProblem(grad_H_ana, x[idx], tspan)
-    data_reference = ODE.solve(prob_reference, Tsit5(), abstol=1e-10, reltol=1e-10, saveat = trange, tstops = trange)
+    prob_reference = ODEProblem((dx, t, x, params) -> dx .= grad_H_ana(x, params, t), tspan, tstep, x[idx])
+    data_reference = integrate(prob_reference, Gauss(2))
 
-    prob_sindy = DifferentialEquations.ODEProblem(vectorfield, x[idx], tspan)
-    data_sindy = ODE.solve(prob_sindy, Tsit5(), abstol=1e-10, reltol=1e-10, saveat = trange, tstops = trange) 
+    prob_sindy = ODEProblem((dx, t, x, params) -> vectorfield(dx, x, params, t), tspan, tstep, x[idx])
+    data_sindy = integrate(prob_sindy, Gauss(2))
 
     p1 = plot(xlabel = "Time", ylabel = "q₁")
-    scatter!(p1, data_reference.t, data_reference[1,:], label = "Data q₁")
-    scatter!(p1, data_sindy.t, data_sindy[1,:], markershape=:xcross, label = "Identified q₁")
+    scatter!(p1, data_reference.t, data_reference.q[:,1], label = "Data q₁")
+    scatter!(p1, data_sindy.t, data_sindy.q[:,1], markershape=:xcross, label = "Identified q₁")
 
     p3 = plot(xlabel = "Time", ylabel = "p₁")
-    scatter!(p3, data_reference.t, data_reference[3,:], label = "Data p₁")
-    scatter!(p3, data_sindy.t, data_sindy[3,:], markershape=:xcross, label = "Identified p₁")
+    scatter!(p3, data_reference.t, data_reference.q[:,3], label = "Data p₁")
+    scatter!(p3, data_sindy.t, data_sindy.q[:,3], markershape=:xcross, label = "Identified p₁")
 
     plot!(size=(1000,1000))
     display(plot(p1, p3, title="Analytical vs Calculated q₁ & p₁ in a 2D system with Euler"))
 
     p2 = plot(xlabel = "Time", ylabel = "q₂")
-    scatter!(p2, data_reference.t, data_reference[2,:], label = "Data q₂")
-    scatter!(p2, data_sindy.t, data_sindy[2,:], markershape=:xcross, label = "Identified q₂")
+    scatter!(p2, data_reference.t, data_reference.q[:,2], label = "Data q₂")
+    scatter!(p2, data_sindy.t, data_sindy.q[:,2], markershape=:xcross, label = "Identified q₂")
 
     p4 = plot(xlabel = "Time", ylabel = "p₂")
-    scatter!(p4, data_reference.t, data_reference[4,:], label = "Data p₂")
-    scatter!(p4, data_sindy.t, data_sindy[4,:], markershape=:xcross, label = "Identified p₂")
+    scatter!(p4, data_reference.t, data_reference.q[:,4], label = "Data p₂")
+    scatter!(p4, data_sindy.t, data_sindy.q[:,4], markershape=:xcross, label = "Identified p₂")
 
     plot!(size=(1000,1000))
     display(plot(p2, p4, title="Analytical vs Calculated q₂ & p₂ in a 2D system with Euler"))
